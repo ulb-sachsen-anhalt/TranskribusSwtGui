d3f27dcb326af434654214ea78dabbe8947d8d6a
kahlep <kahlep@users.noreply.github.com>
2020-11-03 16:08:42 +0100

    CreditManager: grey out expired and depleted packages, add tooltip hint

--- src/main/java/eu/transkribus/swt_gui/pagination_tables/CreditPackagesUserPagedTableWidget.java
+++ src/main/java/eu/transkribus/swt_gui/pagination_tables/CreditPackagesUserPagedTableWidget.java
@@ -1,10 +1,14 @@
 package eu.transkribus.swt_gui.pagination_tables;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
 
 import javax.ws.rs.ServerErrorException;
 
-import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.nebula.widgets.pagination.table.PageableTable;
 import org.eclipse.swt.SWT;
@@ -21,6 +25,7 @@ import eu.transkribus.core.model.beans.rest.TrpCreditPackageList;
 import eu.transkribus.swt.pagination_table.ATableWidgetPagination;
 import eu.transkribus.swt.pagination_table.IPageLoadMethod;
 import eu.transkribus.swt.pagination_table.RemotePageLoaderSingleRequest;
+import eu.transkribus.swt.util.Colors;
 import eu.transkribus.swt.util.Fonts;
 import eu.transkribus.swt.util.Images;
 import eu.transkribus.swt_gui.mainwidget.TrpMainWidget;
@@ -39,6 +44,7 @@ public class CreditPackagesUserPagedTableWidget extends ATableWidgetPagination<T
 	public static final String PACKAGE_SHAREABLE_COL = "Shareable";	
 	public static final String PACKAGE_TYPE_COL = "Type";
 	public static final String PACKAGE_DATE_COL = "Created";
+	public static final String PACKAGE_EXPIRATION_DATE_COL = "Expires";
 	public static final String PACKAGE_ID_COL = "ID";
 	
 	RemotePageLoaderSingleRequest<TrpCreditPackageList, TrpCreditPackage> pageLoader;
@@ -89,37 +95,18 @@ public class CreditPackagesUserPagedTableWidget extends ATableWidgetPagination<T
 
 	@Override
 	protected void createColumns() {
-		createColumn(PACKAGE_NAME_COL, 220, "label", new CellLabelProvider() {
-			@Override
-			public void update(ViewerCell cell) {
-				if (cell.getElement() instanceof TrpCreditPackage) {
-					cell.setText(((TrpCreditPackage)cell.getElement()).getProduct().getLabel());	
-				}
-			}
-		});
-		createDefaultColumn(PACKAGE_BALANCE_COL, 80, "balance", true);
-//		createDefaultColumn(PACKAGE_USER_NAME_COL, 120, "userName", true);
+		createColumn(PACKAGE_NAME_COL, 220, "label", new PackageColumnLabelProvider(p -> p.getProduct().getLabel()));
+		createColumn(PACKAGE_BALANCE_COL, 80, "balance", new PackageColumnLabelProvider(p -> "" + p.getBalance()));
+//		createDefaultColumn(PACKAGE_USER_NAME_COL, 120, "userName", new PackageColumnLabelProvider(p -> "" + p.getUserName()));
 		//for now we don't need the userid
-//		createDefaultColumn(PACKAGE_USER_ID_COL, 50, "userId", true);
-		createColumn(PACKAGE_SHAREABLE_COL, 70, "shareable", new CellLabelProvider() {
-			@Override
-			public void update(ViewerCell cell) {
-				if (cell.getElement() instanceof TrpCreditPackage) {
-					cell.setText(((TrpCreditPackage)cell.getElement()).getProduct().getShareable() + "");	
-				}
-			}
-		});
-		createDefaultColumn(PACKAGE_DATE_COL, 120, "purchaseDate", true);
+//		createDefaultColumn(PACKAGE_USER_ID_COL, 50, "userId", new PackageColumnLabelProvider(p -> "" + p.getUserId()));
+		createColumn(PACKAGE_SHAREABLE_COL, 70, "shareable", new PackageColumnLabelProvider(p -> "" + p.getProduct().getShareable()));
+		createColumn(PACKAGE_DATE_COL, 120, "purchaseDate", new PackageColumnLabelProvider(p -> "" + p.getPurchaseDate()));
 		//hide credit type as the value is currently not used anyway
-//		createColumn(PACKAGE_TYPE_COL, 100, "creditType", new CellLabelProvider() {
-//			@Override
-//			public void update(ViewerCell cell) {
-//				if (cell.getElement() instanceof TrpCreditPackage) {
-//					cell.setText(((TrpCreditPackage)cell.getElement()).getProduct().getCreditType());	
-//				}
-//			}
-//		});
-		createDefaultColumn(PACKAGE_ID_COL, 50, "packageId", true);
+//		createColumn(PACKAGE_TYPE_COL, 100, "creditType", new PackageColumnLabelProvider(p -> "" + p.getCreditType()));
+		createColumn(PACKAGE_EXPIRATION_DATE_COL, 120, "expirationDate", new PackageColumnLabelProvider(p -> "" + p.getExpirationDate()));
+		createColumn(PACKAGE_ID_COL, 50, "packageId", new PackageColumnLabelProvider(p -> "" + p.getPackageId()));
+		ColumnViewerToolTipSupport.enableFor(super.getTableViewer());
 	}
 
 	protected RemotePageLoaderSingleRequest<TrpCreditPackageList, TrpCreditPackage> createPageLoader() {
@@ -192,6 +179,60 @@ public class CreditPackagesUserPagedTableWidget extends ATableWidgetPagination<T
 
 			overallBalanceValueTxt.setText(txt);
 			overallBalanceValueTxt.pack();
+		}
+	}
+	
+	protected static class PackageColumnLabelProvider extends ColumnLabelProvider {
+		
+		Function<TrpCreditPackage, String> getter;
+		Date now;
+		
+		public PackageColumnLabelProvider(Function<TrpCreditPackage, String> getter) {
+			super();
+			this.getter = getter;
+			this.now = new Date();
+		}
+		
+		@Override
+		public void update(ViewerCell cell) {
+			if (!(cell.getElement() instanceof TrpCreditPackage)) {
+				return;
+			}
+			TrpCreditPackage p = (TrpCreditPackage) cell.getElement();
+			cell.setText(getter.apply(p));
+			
+			//show expired packages gray
+			boolean greyOut = p.getExpirationDate() != null && now.after(p.getExpirationDate());
+			//show depleted packages gray
+			greyOut |= p.getBalance() != null && p.getBalance() <= 0.0d;
+			
+			if(greyOut) {
+				cell.setForeground(Colors.getSystemColor(SWT.COLOR_DARK_GRAY));
+			}
+		}
+		
+		@Override
+		public String getToolTipText(Object element) {
+			if (!(element instanceof TrpCreditPackage)) {
+				return super.getToolTipText(element);
+			}
+			TrpCreditPackage p = (TrpCreditPackage) element;
+			List<String> hints = new ArrayList<>(2);
+			if(p.getExpirationDate() != null && now.after(p.getExpirationDate())) {
+				hints.add("expired");
+			}
+			if(p.getBalance() != null && p.getBalance() <= 0.0d) {
+				hints.add("depleted");
+			}
+			
+			if(hints.isEmpty()) {
+				return super.getToolTipText(element);
+			}
+			String msg = "Package is " + hints.get(0);
+			if(hints.size() > 1) {
+				msg += " and " + hints.get(1);
+			}
+			return msg;
 		}
 	}
 }
